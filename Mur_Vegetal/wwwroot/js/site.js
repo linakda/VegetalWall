032fc187fc68cf1140e5dba2fc29b0a5e3001680
garmatueur <remi.vandewalle@isen.yncrea.fr>
2019-05-23 11:13:41 +0200

    Page accueil

--- Mur_Vegetal/wwwroot/js/site.js
+++ Mur_Vegetal/wwwroot/js/site.js
@@ -2,3 +2,85 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function compte_a_rebours()
+{
+    var compte_a_rebours = document.getElementsByClassName("countdown-box")[0];
+
+    var date_actuelle = new Date();
+    var date_evenement = new Date("May 30 11:00:00 2019"); //date de l'événement
+    var total_secondes = (date_evenement - date_actuelle) / 1000;
+
+    var prefixe = "Dans ";
+    if (total_secondes < 0)
+    {
+        prefixe = "Terminé il y a "; // On modifie le préfixe si la différence est négatif
+        total_secondes = Math.abs(total_secondes); // On ne garde que la valeur absolue
+    }
+
+    if (total_secondes > 0)
+    {
+        var jours = Math.floor(total_secondes / (60 * 60 * 24));
+        var heures = Math.floor((total_secondes - (jours * 60 * 60 * 24)) / (60 * 60));
+        minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+        secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
+
+        var mot_jour = "J ";
+        var mot_heure = "H ";
+        var mot_minute = "MIN ";
+        var mot_seconde = "SEC ";
+
+        if (jours == 0)
+        {
+            jours = '';
+            mot_jour = '';
+        }
+        else if (jours == 1)
+        {
+            mot_jour = "jour,";
+        }
+
+        if (heures == 0)
+        {
+            heures = '';
+            mot_heure = '';
+        }
+        else if (heures == 1)
+        {
+            mot_heure = "heure,";
+        }
+
+        if (minutes == 0)
+        {
+            minutes = '';
+            mot_minute = '';
+        }
+        else if (minutes == 1)
+        {
+            mot_minute = "minute,";
+        }
+
+        if (secondes == 0)
+        {
+            secondes = '';
+            mot_seconde = '';
+        }
+        else if (secondes == 1)
+        {
+            mot_seconde = "seconde";
+        }
+
+        if (minutes == 0 && heures == 0 && jours == 0)
+        {
+            et = "";
+        }
+
+        compte_a_rebours.innerHTML = prefixe + jours + mot_jour + ' ' + heures + mot_heure + ' ' + minutes + mot_minute + ' ' + secondes + mot_seconde;
+    }
+    else
+    {
+        compte_a_rebours.innerHTML = 'Compte à rebours terminé.';
+    }
+
+    var actualisation = setTimeout("compte_a_rebours();", 1000);
+    
+}  
