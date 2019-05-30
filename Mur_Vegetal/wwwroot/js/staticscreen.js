1cadfae3fee3cbd1c0562ba34129c1df4e1df171
Mélodie <melodie.eintrazi@isen.yncrea.fr>
2019-05-30 20:44:35 +0200

    defilement

--- Mur_Vegetal/wwwroot/js/staticscreen.js
+++ Mur_Vegetal/wwwroot/js/staticscreen.js
@@ -1,38 +1,36 @@
+var elements = [];
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c");
+a.style.display = "none";
+b.style.display = "none";
+c.style.display = "none";
+elements.push(a, b, c);
 
+async function myFunction() {
 
-function myFunction() {
-    
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
-    var c = document.getElementById("c");
-    a.style.display = "none";
-    b.style.display = "none";
-    c.style.display = "none";
-    
-    if (a.style.display === "none") {
-      a.style.display = "block";
-      b.style.display = "none";
-      c.style.display = "none";
-    } else if(b === "none"){
-      a.style.display = "none";
-      c.style.display = "none";
-      b.style.display = "block";
+  for (let i = 0; i < elements.length; i++) {
+    if (i - 1 < 0) {
+      elements[elements.length - 1].style.display = 'none';
+    } else {
+      elements[i - 1].style.display = 'none';
     }
-    else if (c === "none") {
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "block";
-    }
-   
-  } 
-  
-      
-    setInterval(() => {
-            myFunction();
-        }, 2000);
-        
-      
-  
+    elements[i].style.display = 'block';
+    await wait1Second();
 
-   
+  }
  
+}
+
+function wait1Second(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 1000);
+  });
+}
+
+setInterval(() => {
+  myFunction();
+}, 3000);
+
