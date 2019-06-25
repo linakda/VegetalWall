cb44aaf01961b7e4affbc3d37930d155768ef68a
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-25 20:47:15 +0200

    Add display date verification NEWS

--- Mur_Vegetal/Model/News.cshtml.cs
+++ Mur_Vegetal/Model/News.cshtml.cs
@@ -17,24 +17,19 @@ namespace Mur_Vegetal.Pages
             public int position { get; set; }
             public string id { get; set; }
         }
-        public string Answer { get; private set; }
 
         public string _ResultViewNews { get; private set; }
         public void OnGet(){
-            //Answer = Query.Get("http://iotdata.yhdf.fr/api/web/events");
             var result = JsonConvert.DeserializeObject<List<News>>(Query.Get("http://iotdata.yhdf.fr/api/web/events"));
             _ResultViewNews = "";
             var currentTimeStamp = (Int32)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
             foreach(var e in result){
-                /* if (){
-
+                if (e.beginningDate <= currentTimeStamp && e.endingDate >= currentTimeStamp){
+                    _ResultViewNews += "<div class=\"news-block\"><div class=\"news-image box\"><img src=\"data:image;base64, "+e.eventImage+"\"/></div><div class=\"news-text box\">"+e.text+"</div></div>";
                 }
                 else {
-
-                }*/
-                _ResultViewNews += "<div class=\"news-block\"><div class=\"news-image box\"><img src=\"data:image;base64, "+e.eventImage+"\"/></div><div class=\"news-text box\">"+e.text+"</div></div>";
+                }
             }
-            _ResultViewNews = currentTimeStamp.ToString();;
         }
     }
 }
