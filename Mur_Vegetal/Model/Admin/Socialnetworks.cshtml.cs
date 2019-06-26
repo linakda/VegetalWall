eb94231d0136f4b03fa309cd94c2c5ce3c0a7bd5
Marine <marine.clarisse@isen.yncrea.fr>
2019-06-26 17:07:13 +0200

    Social Admin error

--- Mur_Vegetal/Model/Admin/Socialnetworks.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Socialnetworks.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using System;
 
 
 namespace Mur_Vegetal.Pages
@@ -18,10 +19,17 @@ namespace Mur_Vegetal.Pages
 
         public void OnGet()
         {
-            var result = JsonConvert.DeserializeObject<List<Social>>(Query.Get("http://iotdata.yhdf.fr/api/web/socials"));
-            _ResultViewAdminSocialnetworks = "";
-            foreach(var e in result){
-                _ResultViewAdminSocialnetworks += "<div class=\"home-param\"> <legend class=\"home-name\"> Page : " +e.pageWidget + "</legend> <div class=\"page-param\"> <label class=\"page-param\">Nom de page : </label> <input type=\"text\" class=\"page-param\" placeholder=\"Nom de la page \" value=\" " + e.pageWidget + "  \"> </div> <div class=\"widget-param\"> <label class=\"widget-param\">Lien widget : </label> <input type=\"text\" class=\"widget-param\" placeholder=\"Lien du widget \" value=\" " +e.widget + " \"> </div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
+
+            var requestSocial = Query.Get("http://iotdata.yhdf.fr/api/web/socials");
+            if(requestSocial == "Error" || String.IsNullOrEmpty(requestSocial)){
+                _ResultViewAdminSocialnetworks = "<div class=\"home-param\">Error Api </div> ";
+            }
+            else {
+                var result = JsonConvert.DeserializeObject<List<Social>>(requestSocial);
+                _ResultViewAdminSocialnetworks = "";
+                foreach(var e in result){
+                    _ResultViewAdminSocialnetworks += "<div class=\"home-param\"> <legend class=\"home-name\"> Page : " +e.pageWidget + "</legend> <div class=\"page-param\"> <label class=\"page-param\">Nom de page : </label> <input type=\"text\" class=\"page-param\" placeholder=\"Nom de la page \" value=\" " + e.pageWidget + "  \"> </div> <div class=\"widget-param\"> <label class=\"widget-param\">Lien widget : </label> <input type=\"text\" class=\"widget-param\" placeholder=\"Lien du widget \" value=\" " +e.widget + " \"> </div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
+                }
             }
         }
     }
