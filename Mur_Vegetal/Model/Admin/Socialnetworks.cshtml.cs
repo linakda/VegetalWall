1fc7d09a5355cdb88cd9c58548c70743bcae05b6
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-26 22:56:44 +0200

    New GET method SOCIALADMIN

--- Mur_Vegetal/Model/Admin/Socialnetworks.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Socialnetworks.cshtml.cs
@@ -15,21 +15,17 @@ namespace Mur_Vegetal.Pages
             public string widget { get; set; }
             public string id { get; set; }
         }
-        public string _ResultViewAdminSocialnetworks { get; private set; }
-
-        public void OnGet()
-        {
-
-            var requestSocial = Query.Get("http://iotdata.yhdf.fr/api/web/socials");
-            if(requestSocial == "Error" || String.IsNullOrEmpty(requestSocial)){
-                _ResultViewAdminSocialnetworks = "<div class=\"home-param\">Error Api </div> ";
+        
+        public List <Social> Result { get; private set; }
+        public bool IsError { get; private set; }
+        public void OnGet(){
+            var requestSocialnetworks = Query.Get("http://iotdata.yhdf.fr/api/web/socials");
+            if(requestSocialnetworks == "Error" || String.IsNullOrEmpty(requestSocialnetworks)){
+                IsError = true;
             }
-            else {
-                var result = JsonConvert.DeserializeObject<List<Social>>(requestSocial);
-                _ResultViewAdminSocialnetworks = "";
-                foreach(var e in result){
-                    _ResultViewAdminSocialnetworks += "<div class=\"home-param\"> <legend class=\"home-name\"> Page : " +e.pageWidget + "</legend> <div class=\"page-param\"> <label class=\"page-param\">Nom de page : </label> <input type=\"text\" class=\"page-param\" placeholder=\"Nom de la page \" value=\" " + e.pageWidget + "  \"> </div> <div class=\"widget-param\"> <label class=\"widget-param\">Lien widget : </label> <input type=\"text\" class=\"widget-param\" placeholder=\"Lien du widget \" value=\" " +e.widget + " \"> </div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
-                }
+            else{
+                IsError = false;
+                Result = JsonConvert.DeserializeObject<List<Social>>(requestSocialnetworks); 
             }
         }
     }
