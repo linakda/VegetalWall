8afde03c840fe5c9500978149d4c1d83a4cd7736
Marine <marine.clarisse@isen.yncrea.fr>
2019-06-26 17:09:59 +0200

    New Admin error

--- Mur_Vegetal/Model/Admin/News.cshtml.cs
+++ Mur_Vegetal/Model/Admin/News.cshtml.cs
@@ -18,13 +18,20 @@ namespace Mur_Vegetal.Pages
         }
         public string _ResultViewAdminNews { get; private set; }
         public void OnGet(){
-            var result = JsonConvert.DeserializeObject<List<News>>(Query.Get("http://iotdata.yhdf.fr/api/web/events"));
-            _ResultViewAdminNews = "";
-            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
-            foreach(var e in result){
-                var endingDate = epoch.AddSeconds(e.endingDate).ToString("yyyy-MM-dd");
-                var beginningDate = epoch.AddSeconds(e.beginningDate).ToString("yyyy-MM-dd");
-                _ResultViewAdminNews += "<div class=\"news-param\"> <legend class=\"news-name\">"+e.name+"</legend> <div class=\"param-name\"> <label class=\"param-name\">Nom de l'événement : </label> <input type=\"text\" class=\"param-name\" placeholder=\"Ex: JPO\" value=\""+e.name+"\"> </div> <div class=\"param-text\"> <label class=\"param-text\">Texte : </label> <textarea class=\"param-text\" rows=\"5\" cols=\"60\" placeholder=\"Entrez votre texte\">"+e.text+"</textarea> </div> <div class=\"param-img\"> <label class=\"param-img\">Image : </label> <img class=\"param-img\" src=\"data:image/png;base64, " +e.eventImage + "\" alt=" + e.name + " ><span><img src=\"data:image/png;base64, "+e.eventImage+"\" alt=\""+e.name+"\"></span> </div> <div class=\"param-start-date\"> <label class=\"param-start-date\">Date de début : </label> <input class=\"param-start-date\" type=\"date\" value=\""+beginningDate+"\"> </div> <div class=\"param-end-date\"> <label class=\"param-end-date\">Date de fin : </label> <input class=\"param-end-date\" type=\"date\" value=\""+endingDate+"\"> </div> <div class=button> <button class=\"button-delete\"> Supprimer </button> <button class=\"button-apply\"> Valider </button> </div> </div>";
+
+            var requestNew = Query.Get("http://iotdata.yhdf.fr/api/web/events");
+            if(requestNew == "Error" || String.IsNullOrEmpty(requestNew)){
+                _ResultViewAdminNews = "<div class=\"news-param\">Error Api</div>";
+            }
+            else{
+                var result = JsonConvert.DeserializeObject<List<News>>(requestNew);
+                _ResultViewAdminNews = "";
+                DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+                foreach(var e in result){
+                    var endingDate = epoch.AddSeconds(e.endingDate).ToString("yyyy-MM-dd");
+                    var beginningDate = epoch.AddSeconds(e.beginningDate).ToString("yyyy-MM-dd");
+                    _ResultViewAdminNews += "<div class=\"news-param\"> <legend class=\"news-name\">"+e.name+"</legend> <div class=\"param-name\"> <label class=\"param-name\">Nom de l'événement : </label> <input type=\"text\" class=\"param-name\" placeholder=\"Ex: JPO\" value=\""+e.name+"\"> </div> <div class=\"param-text\"> <label class=\"param-text\">Texte : </label> <textarea class=\"param-text\" rows=\"5\" cols=\"60\" placeholder=\"Entrez votre texte\">"+e.text+"</textarea> </div> <div class=\"param-img\"> <label class=\"param-img\">Image : </label> <img class=\"param-img\" src=\"data:image/png;base64, " +e.eventImage + "\" alt=" + e.name + " ><span><img src=\"data:image/png;base64, "+e.eventImage+"\" alt=\""+e.name+"\"></span> </div> <div class=\"param-start-date\"> <label class=\"param-start-date\">Date de début : </label> <input class=\"param-start-date\" type=\"date\" value=\""+beginningDate+"\"> </div> <div class=\"param-end-date\"> <label class=\"param-end-date\">Date de fin : </label> <input class=\"param-end-date\" type=\"date\" value=\""+endingDate+"\"> </div> <div class=button> <button class=\"button-delete\"> Supprimer </button> <button class=\"button-apply\"> Valider </button> </div> </div>";
+                }
             }
         }
     }
