f60b75a5bc46df6b3f2ad6af23f7d124f16eb3f1
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-26 22:35:12 +0200

    New GET method FRAMEADMIN

--- Mur_Vegetal/Model/Admin/Frame.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Frame.cshtml.cs
@@ -12,37 +12,17 @@ namespace Mur_Vegetal.Pages{
             public int carrousselTime { get; set; }
             public string id { get; set; }
         }
-        public string _ResultViewAdminFrame {get; private set;}
 
+        public List <Frame> Result { get; private set; }
+        public bool IsError { get; private set; }
         public void OnGet(){
-
             var requestFrame = Query.Get("http://iotdata.yhdf.fr/api/web/tables");
-            if(requestFrame =="Error" || String.IsNullOrEmpty(requestFrame)){
-                _ResultViewAdminFrame = "<div class=\"frame-param \"> Error Api </div> ";
+            if(requestFrame == "Error" || String.IsNullOrEmpty(requestFrame)){
+                IsError = true;
             }
-            else {
-                var result = JsonConvert.DeserializeObject<List<Frame>>(requestFrame);
-                _ResultViewAdminFrame = "";
-                foreach(var e in result){
-                    if (e.name == "wall" || e.name == "socialnetworks" || e.name == "countdown"){
-                        if (e.isOnScreen == true){
-                            _ResultViewAdminFrame += "<div class=\"frame-param\"><legend class=\"frame-name\">"+e.name+"</legend><div class=\"param-disp\"><label class=\"param-disp\">Affichage </label><input type=\"checkbox\" class=\"param-disp\" checked></div><div class=\"param-disptime\"><label class=\"param-disptime\">Temps d'affichage : </label><input list=\"Sec\" type=\"text\" class=\"param-disptime\" placeholder=\"En secondes\" value=\""+e.onScreenTime+"\"><datalist id=\"Sec\"><option value=\"10\"></option><option value=\"20\"></option><option value=\"30\"></option><option value=\"40\"></option><option value=\"50\"></option><option value=\"60\"></option><option value=\"120\"></option></datalist></div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
-                        }
-                        else {
-                            _ResultViewAdminFrame += "<div class=\"frame-param\"><legend class=\"frame-name\">"+e.name+"</legend><div class=\"param-disp\"><label class=\"param-disp\">Affichage </label><input type=\"checkbox\" class=\"param-disp\"></div><div class=\"param-disptime\"><label class=\"param-disptime\">Temps d'affichage : </label><input list=\"Sec\" type=\"text\" class=\"param-disptime\" placeholder=\"En secondes\" value=\""+e.onScreenTime+"\"><datalist id=\"Sec\"><option value=\"10\"></option><option value=\"20\"></option><option value=\"30\"></option><option value=\"40\"></option><option value=\"50\"></option><option value=\"60\"></option><option value=\"120\"></option></datalist></div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
-                        }
-                    }
-                    else if (e.name == "medias" || e.name == "news"){
-                        if (e.isOnScreen == true){
-                            _ResultViewAdminFrame += "<div class=\"frame-param\"><legend class=\"frame-name\">"+e.name+"</legend><div class=\"param-disp\"><label class=\"param-disp\">Affichage </label><input type=\"checkbox\" class=\"param-disp\" checked></div> <div class=\"param-carroussel\"> <label class=\"param-carroussel\">Temps d'affichage du carroussel : </label> <input list=\"Sec\" type=\"text\" class=\"param-carroussel\" placeholder=\"En secondes\" value=\""+e.carrousselTime+"\"> <datalist id=\"Sec\"> <option value=\"10\"></option> <option value=\"20\"></option> <option value=\"30\"></option> <option value=\"40\"></option> <option value=\"50\"></option> <option value=\"60\"></option> <option value=\"120\"></option> </datalist> </div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
-                        }
-                        else {
-                            _ResultViewAdminFrame += "<div class=\"frame-param\"><legend class=\"frame-name\">"+e.name+"</legend><div class=\"param-disp\"><label class=\"param-disp\">Affichage </label><input type=\"checkbox\" class=\"param-disp\"></div><div class=\"param-carroussel\"> <label class=\"param-carroussel\">Temps d'affichage du carroussel : </label> <input list=\"Sec\" type=\"text\" class=\"param-carroussel\" placeholder=\"En secondes\" value=\""+e.carrousselTime+"\"> <datalist id=\"Sec\"> <option value=\"10\"></option> <option value=\"20\"></option> <option value=\"30\"></option> <option value=\"40\"></option> <option value=\"50\"></option> <option value=\"60\"></option> <option value=\"120\"></option> </datalist> </div> <div class=button> <button class=\"button-apply\"> Valider </button> </div> </div>";
-                        }
-                    }
-                    else {
-                    }
-                }
+            else{
+                IsError = false;
+                Result = JsonConvert.DeserializeObject<List<Frame>>(requestFrame); 
             }
         }
     }
