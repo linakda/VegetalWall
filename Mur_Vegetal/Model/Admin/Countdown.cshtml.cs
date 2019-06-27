ff8c5f2cce43bc1605441307150468e1632ad8de
Marine <marine.clarisse@isen.yncrea.fr>
2019-06-27 11:50:17 +0200

    CaR date

--- Mur_Vegetal/Model/Admin/Countdown.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Countdown.cshtml.cs
@@ -13,7 +13,7 @@ namespace Mur_Vegetal.Pages
             public string name { get; set; }
             public int endingDateEvent { get; set; }
             public int beginningDateEvent { get; set; }
-            public int endingDateCountdown { get; set; }
+            public double endingDateCountdown { get; set; }
             public int position { get; set; }
             public object image { get; set; }
             public string id { get; set; }
@@ -38,11 +38,11 @@ namespace Mur_Vegetal.Pages
         public void OnPost(){
             var name = Request.Form["name"];
             var text = Request.Form["text"];
-            var startdate = ((DateTimeOffset)DateTime.Parse(Request.Form["startdate"])).ToUnixTimeSeconds();
-            var enddate = ((DateTimeOffset)DateTime.Parse(Request.Form["enddate"])).ToUnixTimeSeconds();
-            var countdowndate = 0;//transform string to date
-            var countdowntime = 0;//transform string to time
-            var totalcountdown = 0; //mix date + time
+            var startdate = (((DateTimeOffset)DateTime.Parse(Request.Form["startdate"])).Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var enddate = (((DateTimeOffset)DateTime.Parse(Request.Form["enddate"])).Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var countdowndate = (((DateTimeOffset)DateTime.Parse(Request.Form["countdowndate"])).Subtract(new DateTime(1970, 1, 1))).TotalSeconds;//transform string to date
+            var countdowntime = TimeSpan.Parse(Request.Form["countdowntime"]).TotalSeconds;//transform string to time
+            var totalcountdown = countdowndate + countdowntime; //mix date + time
             var submit = Request.Form["submit"];
             var id = Request.Form["id"];
             if(submit == "add"){
@@ -52,14 +52,14 @@ namespace Mur_Vegetal.Pages
                 toAdd.endingDateEvent = Convert.ToInt32(enddate);
                 toAdd.endingDateCountdown = totalcountdown;
                 toAdd.text = text;
-                if (ImageUpload != null){
+                if(ImageUpload != null){
                     using (var ms = new MemoryStream()){
                     ImageUpload.CopyTo(ms);
                     var fileBytes = ms.ToArray();
                     toAdd.image = Convert.ToBase64String(fileBytes);
                     }
                 }
-                else {
+                else{
                     toAdd.image = "";
                 }
                 var data =  JsonConvert.SerializeObject(toAdd);
@@ -70,10 +70,11 @@ namespace Mur_Vegetal.Pages
                 CountDown toEdit = new CountDown();
                 if (!String.IsNullOrEmpty(image)){
                     toEdit.image = image;
-                };
+                }
                 else {
                     toEdit.image = "";
                 }
+                Console.Write(toEdit.endingDateCountdown);
                 toEdit.text = text;
                 toEdit.name = name;
                 toEdit.beginningDateEvent = Convert.ToInt32(startdate);
@@ -83,7 +84,7 @@ namespace Mur_Vegetal.Pages
                 var data =  JsonConvert.SerializeObject(toEdit);
                 var result = Query.Put("http://iotdata.yhdf.fr/api/web/countdowns/"+toEdit.id,data);
             }
-            else if (submit == "delete"){
+            else if(submit == "delete"){
                 var result = Query.Delete("http://iotdata.yhdf.fr/api/web/countdowns/"+id);
             }
             OnGet();
