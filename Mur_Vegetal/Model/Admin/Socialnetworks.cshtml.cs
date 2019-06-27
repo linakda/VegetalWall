35711027650d3b93a6c7ead6396e196ed34cd573
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-27 09:13:59 +0200

    Edit SOCIALS

--- Mur_Vegetal/Model/Admin/Socialnetworks.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Socialnetworks.cshtml.cs
@@ -26,5 +26,23 @@ namespace Mur_Vegetal.Pages
                 Result = JsonConvert.DeserializeObject<List<Social>>(requestSocialnetworks); 
             }
         }
+
+        public void OnPost(){
+            var username = Request.Form["username"];
+            var pageWidget = Request.Form["pagewidget"];
+            var widget = Request.Form["widget"];
+            var submit = Request.Form["submit"];
+            var id = Request.Form["id"];
+            if(submit == "edit"){
+                Social toEdit = new Social();
+                toEdit.widget = widget;
+                toEdit.pageWidget = pageWidget;
+                toEdit.username = username;
+                toEdit.id = id;
+                var data =  JsonConvert.SerializeObject(toEdit);
+                var result = Query.Put("http://iotdata.yhdf.fr/api/web/socials/"+toEdit.id,data);
+            }
+            OnGet();
+        }
     }
 }
