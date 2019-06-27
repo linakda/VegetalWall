d7ac7c746f68d62ed57f865aecf54a8bc9cfdd25
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-27 09:08:29 +0200

    Edit FRAMES

--- Mur_Vegetal/Model/Admin/Frame.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Frame.cshtml.cs
@@ -25,5 +25,33 @@ namespace Mur_Vegetal.Pages{
                 Result = JsonConvert.DeserializeObject<List<Frame>>(requestFrame); 
             }
         }
+
+        public void OnPost(){
+            var isOnScreen = Request.Form["isOnScreen"];
+            var submit = Request.Form["submit"];
+            var time = Request.Form["time"];
+            var name = Request.Form["name"];
+            var id = Request.Form["id"];
+            if(submit == "edit"){
+                Frame toEdit = new Frame();
+                if (name == "news" || name == "medias"){
+                    toEdit.carrousselTime = Int32.Parse(time);
+                }
+                else if (name == "countdown" || name == "socialnetworks" || name == "wall"){
+                    toEdit.onScreenTime = Int32.Parse(time);
+                }
+                if (isOnScreen == "on"){
+                    toEdit.isOnScreen = true;
+                }
+                else {
+                    toEdit.isOnScreen = false;
+                }
+                toEdit.name = name;
+                toEdit.id = id;
+                var data =  JsonConvert.SerializeObject(toEdit);
+                var result = Query.Put("http://iotdata.yhdf.fr/api/web/tables/"+toEdit.id,data);
+            }
+            OnGet();
+        }
     }
 }
