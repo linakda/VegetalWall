c685c690951d98c1eaabe8c15799c60cead8f1f1
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-27 15:54:46 +0200

    start cookie admin

--- Mur_Vegetal/Model/Admin/Login.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Login.cshtml.cs
@@ -1,17 +1,47 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
 
 namespace Mur_Vegetal.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
-        {
-
+        public void OnGet(){
+            if( Request.Cookies["communication"] != null ){
+                Redirect("/Admin");
+            }
+            if( Request.Cookies["administration"] != null ){
+                Redirect("/AdminWall");
+            }
         }
+
+        public void OnPost(){
+            var login = Request.Form["login"];
+            var password = Request.Form["password"];
+            var submit = Request.Form["submit"];
+            if (submit == "connexion"){
+                Console.WriteLine("LOGIN");
+                if (login == "communication"){
+                    if (password == "AdminComm2019"){
+                        var cookieOptions = new CookieOptions{
+                            Expires = DateTime.Now.AddHours(2)
+                        };
+                        //Response.Cookies.Append("communication", "ok", cookieOptions)
+                        Console.WriteLine("COMMM");
+                    }
+                }
+                if (login == "administration"){
+                    if (password == "AdminAdmin2019"){
+                        var cookieOptions = new CookieOptions{
+                            Expires = DateTime.Now.AddHours(2)
+                        };
+                        Response.Cookies.Append("administration", "ok", cookieOptions);
+                        Console.WriteLine("ADMIN");
+                    }
+                }
+            }
+            OnGet();
+        }
+
     }
 }
