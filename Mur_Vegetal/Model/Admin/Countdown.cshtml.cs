0bc8233d7ea745243c59f6456315269067a5ec54
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-27 09:34:56 +0200

    Add/Delete/Edit COUNTDOWN

--- Mur_Vegetal/Model/Admin/Countdown.cshtml.cs
+++ Mur_Vegetal/Model/Admin/Countdown.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Mur_Vegetal.Pages
 {
@@ -19,6 +21,8 @@ namespace Mur_Vegetal.Pages
 
          public List <CountDown> Result { get; private set; }
 
+         public IFormFile ImageUpload { get; set; }
+
          public bool IsError { get; private set; }
         public void OnGet(){
             var requestCountdown = Query.Get("http://iotdata.yhdf.fr/api/web/countdowns");
@@ -29,6 +33,60 @@ namespace Mur_Vegetal.Pages
                 IsError = false;
                 Result = JsonConvert.DeserializeObject<List<CountDown>>(requestCountdown); 
             }
+        }
+
+        public void OnPost(){
+            var name = Request.Form["name"];
+            var text = Request.Form["text"];
+            var startdate = ((DateTimeOffset)DateTime.Parse(Request.Form["startdate"])).ToUnixTimeSeconds();
+            var enddate = ((DateTimeOffset)DateTime.Parse(Request.Form["enddate"])).ToUnixTimeSeconds();
+            var countdowndate = 0;//transform string to date
+            var countdowntime = 0;//transform string to time
+            var totalcountdown = 0 //mix date + time
+            var submit = Request.Form["submit"];
+            var id = Request.Form["id"];
+            if(submit == "add"){
+                CountDown toAdd = new CountDown();
+                toAdd.name = name;
+                toAdd.beginningDateEvent = Convert.ToInt32(startdate);
+                toAdd.endingDateEvent = Convert.ToInt32(enddate);
+                toAdd.endingDateCountdown = totalcountdown;
+                toAdd.text = text;
+                if (ImageUpload != null){
+                    using (var ms = new MemoryStream()){
+                    ImageUpload.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+                    toAdd.image = Convert.ToBase64String(fileBytes);
+                    }
+                }
+                else {
+                    toAdd.image = "";
+                }
+                var data =  JsonConvert.SerializeObject(toAdd);
+                var result = Query.Post("http://iotdata.yhdf.fr/api/web/countdowns/",data);
+            }
+            else if(submit == "edit"){
+                var image = Request.Form["image"];
+                CountDown toEdit = new CountDown();
+                if (!String.IsNullOrEmpty(image)){
+                    toEdit.image = image;
+                };
+                else {
+                    toEdit.image = "";
+                }
+                toEdit.text = text;
+                toEdit.name = name;
+                toEdit.beginningDateEvent = Convert.ToInt32(startdate);
+                toEdit.endingDateEvent = Convert.ToInt32(enddate);
+                toEdit.endingDateCountdown = totalcountdown;
+                toEdit.id = id;
+                var data =  JsonConvert.SerializeObject(toEdit);
+                var result = Query.Put("http://iotdata.yhdf.fr/api/web/countdowns/"+toEdit.id,data);
+            }
+            else if (submit == "delete"){
+                var result = Query.Delete("http://iotdata.yhdf.fr/api/web/countdowns/"+id);
+            }
+            OnGet();
         }
     }
 }
