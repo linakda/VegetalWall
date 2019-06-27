de0d33d2501e51722a202c7e4831e242d657b7f5
Marine <marine.clarisse@isen.yncrea.fr>
2019-06-27 13:50:06 +0200

    CaR site

--- Mur_Vegetal/Model/Socialnetworks.cshtml.cs
+++ Mur_Vegetal/Model/Socialnetworks.cshtml.cs
@@ -14,22 +14,16 @@ namespace Mur_Vegetal.Pages
             public string widget { get; set; }
             public string id { get; set; }
         }
-        public string _ResultViewSocialnetworks {get; private set;}
-        public void OnGet()
-        {
-            var requestSocial = Query.Get("http://iotdata.yhdf.fr/api/web/socials");
-            if(requestSocial=="Error" || String.IsNullOrEmpty(requestSocial)){
-                _ResultViewSocialnetworks = "<div style=\"color:green; \"> Error Api </div>";
+        public List <Social> Result { get; private set; }
+        public bool IsError { get; private set; }
+        public void OnGet(){
+            var requestSocialnetworks = Query.Get("http://iotdata.yhdf.fr/api/web/socials");
+            if(requestSocialnetworks == "Error" || String.IsNullOrEmpty(requestSocialnetworks)){
+                IsError = true;
             }
             else{
-                var result = JsonConvert.DeserializeObject<List<Social>>(requestSocial);
-                _ResultViewSocialnetworks = "";
-                foreach(var e in result){
-                    if(e.pageWidget == "socialnetworks"){
-                        _ResultViewSocialnetworks += "<script src=\"https://snapwidget.com/js/snapwidget.js\"></script> <iframe src=\" " + e.widget + " \" class=\"snapwidget-widget\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"no\" style=\"border:none; overflow:hidden; width:100%;\"></iframe>";
-
-                    }
-                }
+                IsError = false;
+                Result = JsonConvert.DeserializeObject<List<Social>>(requestSocialnetworks); 
             }
         }
     }
